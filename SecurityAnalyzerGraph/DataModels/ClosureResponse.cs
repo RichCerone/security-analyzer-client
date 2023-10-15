0ec4fd177c91cc4aa14f8641c3ef490af118ec78
Rich Cerone <rcerone@live.com>
2023-10-15 15:03:33 -0400

    Added documentation.

--- SecurityAnalyzerGraph/DataModels/ClosureResponse.cs
+++ SecurityAnalyzerGraph/DataModels/ClosureResponse.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace SecurityAnalyzerGraph.DataModels
 {
+    /// <summary>
+    /// Holds details on when a CVE was closed
+    /// for a repo.
+    /// </summary>
     internal class ClosureResponse
     {
+        /// <summary>
+        /// The name of the repo.
+        /// </summary>
         public string RepoName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The CVE where the vulnerability was closed.
+        /// </summary>
         public string Cve { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The time it took to close in days.
+        /// </summary>
         public double TimeToClose { get; set; }
     }
 }
